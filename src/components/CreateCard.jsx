98762432b935eb2db2c16237b6920bbc817ec01d
killervasif <vasifbabazade2004@gmail.com>
2024-01-24 18:21:47 +0400

    Task 5

--- src/components/CreateCard.jsx
+++ src/components/CreateCard.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
+import { useState } from 'react'
+import { v4 as uuidv4 } from "uuid"
 
-function CreateCard({ createVisibility, setCreateVisibility }) {
+function CreateCard({ dispatch, setCards, email }) {
+    const [formData, setFormData] = useState({})
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormData((prevForm) => ({
+            ...prevForm,
+            [name]: value,
+            author: email,
+            id: uuidv4(),
+        }))
+    }
+
+    const createCard = (e) => {
+        e.preventDefault();
+        setCards((prevValue) => [...prevValue, formData])
+        dispatch({ type: 'reset' })
+    }
+
     return (
-        <div className={`${createVisibility ? 'fixed inset-0' : 'hidden'} flex justify-center items-center w-screen z-10 bg-opacity-50 bg-black`}>            
-            <div className='flex flex-col justify-center sm:justify-around px-5 sm:px-20 py-10 items-center w-screen sm:w-[700px] h-screen sm:max-h-[420px] sm:rounded-[13px] bg-white relative'>   
-                <button onClick={() => setCreateVisibility(false)} class="w-5 h-5 bg-red-600 rounded-full absolute top-[10px] right-[10px]"></button>
-                <h3 className='text-2xl font-bold'>CREATE CARD</h3>
-                <div className='flex text-center mt-5 sm:text-left flex-col w-full'>
-                    <label className='text-xl text-zinc-400' htmlFor="">Title</label>
-                    <input className='py-3 px-2 mt-2 border rounded-[6px]' type="text" name="" id="" />
-                    <label className='text-xl mt-2 text-zinc-400' htmlFor="">Description</label>
-                    <input className='py-3 px-2 mt-2 border rounded-[6px]' type="text" name="" id="" />
-                </div>
-                <div className='flex justify-center sm:justify-end sm:items-center w-full mt-5'>
-                <button onClick={() => setCreateVisibility(false)} className='px-5 py-3 border rounded-[15px]  mx-2 bg-white hover:bg-[#DFDFDF]'>Close</button>
-                    <button className='px-5 py-3 border rounded-[15px] mx-5 bg-amber-400 hover:bg-amber-500'>Create</button>
-                </div>
+        <form className='flex flex-col justify-center sm:justify-around px-5 sm:px-20 py-10 items-center w-screen sm:w-[700px] h-screen sm:max-h-[420px] sm:rounded-[13px] bg-white relative'>
+            <button onClick={() => dispatch({ type: 'reset' })} className="w-5 h-5 bg-red-600 rounded-full absolute top-[10px] right-[10px]"></button>
+            <h3 className='text-2xl font-bold'>CREATE CARD</h3>
+            <div className='flex text-center mt-5 sm:text-left flex-col w-full'>
+                <label className='text-xl text-zinc-400' htmlFor="title">Title</label>
+                <input onChange={(e) => handleChange(e)} name='title' required className='py-3 px-2 mt-2 border rounded-[6px]' type="text" id="" />
+                <label className='text-xl mt-2 text-zinc-400' htmlFor="description">Description</label>
+                <input onChange={(e) => handleChange(e)} name='description' required className='py-3 px-2 mt-2 border rounded-[6px]' type="text" id="" />
             </div>
-        </div>
+            <div className='flex justify-center sm:justify-end sm:items-center w-full mt-5'>
+                <button onClick={() => dispatch({ type: 'reset' })} className='px-5 py-3 border rounded-[15px]  mx-2 bg-white hover:bg-[#DFDFDF]'>Close</button>
+                <button onClick={(e) => { createCard(e) }} className='px-5 py-3 border rounded-[15px] mx-5 bg-amber-400 hover:bg-amber-500'>Create</button>
+            </div>
+        </form>
     )
 }
-
-export default CreateCard
--- src/mainpage/components/TodoCard.jsx
+++ src/mainpage/components/TodoCard.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 
-function TodoCard({ setDeleteVisibility, setEditVisibility }) {
+function TodoCard({ dispatch, setActiveCard, data }) {
     return (
-        <div className='w-full h-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6  sm:min-h-[330px] rounded-[20px] m-2 sm:m-3 md:m-4 lg:m-6 xl:m-8'>
+        <div className='w-full h-full my-2 mx-0 sm:mx-3 sm:w-auto sm:min-h-[330px] rounded-[20px] '>
             <div className='flex py-5 flex-col rounded-t-[7px] h-auto items-center justify-center px-3 bg-zinc-200'>
                 <div className='w-full text-center sm:text-left'>
-                    <h3 className='text-3xl font-bold'>Card title</h3>
+                <h3 className='text-3xl truncate font-bold'>{data.title}</h3>
                 </div>
-                <p className='my-1 text-center sm:text-left'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                    nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                    anim id est laborum.
+                <p className='my-1 w-full truncate text-center sm:text-left'>{data.description}
                 </p>
             </div>
             <div className='flex justify-center flex-col sm:flex-row sm:justify-end items-center rounded-b-[7px] p-4 bg-zinc-300 h-auto'>
-                <button onClick={() => setEditVisibility(true)} className='mx-3 sm:mx-1 text-xl w-full sm:w-auto font-bold rounded-[7px] h-3/5 py-2 px-6 bg-[#f6b819] hover:bg-yellow-700'>Edit</button>
-                <button onClick={() => setDeleteVisibility(true)} className='mx-3 sm:mx-1 mt-4 sm:mt-0 text-xl w-full sm:w-auto font-bold rounded-[7px] h-3/5 py-2 px-5 bg-[#f6b819] hover:bg-yellow-700'>Delete</button>
+                <button onClick={() => {
+                    setActiveCard(data)
+                    dispatch({type: 'edit'})
+                }} className='mx-3 sm:mx-1 text-xl w-full sm:w-auto font-bold rounded-[7px] h-3/5 py-2 px-6 bg-[#f6b819] hover:bg-yellow-700'>Edit</button>
+                <button onClick={() => {
+                    setActiveCard(data)
+                    dispatch({type: 'delete'})
+                }} className='mx-3 sm:mx-1 mt-4 sm:mt-0 text-xl w-full sm:w-auto font-bold rounded-[7px] h-3/5 py-2 px-5 bg-[#f6b819] hover:bg-yellow-700'>Delete</button>
             </div>
         </div>
     )
 }
-
 export default TodoCard
